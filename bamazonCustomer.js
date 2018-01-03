bfeb767740137ac89b6690018ef5d9fbb304f2a5
Peter Le <Peter Le@LC-PETERLAPTOP.lec.corp.com>
2018-01-02 22:08:25 -0600

    completed Minimun Req with Screen Shots

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,5 +1,6 @@
 var mysql = require('mysql');
 var inquirer = require('inquirer')
+var bamazonDB = []
 
 var connection = mysql.createConnection({
 	host: "localhost",
@@ -22,13 +23,14 @@ function displayInventory(){
 				console.log("------------------------------");
 				console.log("Product ID: " + results.item_id + " Product Name: " + results.product_name + " Department: " + results.department_name + " Price: $" + results.price + " Stock Quantity: " + results.stock_quantity);
 				console.log("------------------------------");
+				bamazonDB.push(results)
 			});
-			purchaseItem();
+			purchaseItem(results);
   });
 }
 
 
-function purchaseItem() {
+function purchaseItem(results) {
   inquirer.prompt([{
       name: "productID",
       type: "input",
@@ -38,27 +40,33 @@ function purchaseItem() {
 			name: "quantity",
 			type: "input",
 			message: "How many do you want to buy?",
-    	}]).then(function(answers){
-				updateQuantity(answers.productID, answers.quantity);
+		}]).then(function(answers){
+				var buyQuantity = answers.quantity;
+				var arrayIndex = parseInt(answers.productID) - 1;
+				console.log("You are buying " + buyQuantity + " " + bamazonDB[arrayIndex].product_name + " at $" + bamazonDB[arrayIndex].price + " each.")
+				updateQuantity(answers.productID, answers.quantity, arrayIndex);
 	});
 }
 
-function updateQuantity(id, quantity){
+function updateQuantity(id, quantity, arrayIndex){
+	var purchaseQuantity = parseInt(quantity);
+	var newStockCount = bamazonDB[arrayIndex].stock_quantity - purchaseQuantity;
+	var totalCost = bamazonDB[arrayIndex].price * quantity;
+	if(newStockCount >= 0) {
 	var query = connection.query("UPDATE products SET ? WHERE ?",
 		[{
-			var newStockQuantity = stock_quantity;
-			newStockQuantity = newStockQuantity - quantity;
+			stock_quantity: newStockCount
 		},
 		{
-			item_id = id
+			item_id: parseInt(id)
 		}],
 		function(err, results) {
-			displayInventory();
+			if (err) throw err;
+			console.log("You have ordered " + quantity + " " + bamazonDB[arrayIndex].product_name + ". Your total cost is: $" + totalCost);
+			connection.end();
 		});
-}
-
-function totalCost(){
-	var query = connection.query(
-		
-	)
+	} else {
+			console.log("Not enough " + bamazonDB[arrayIndex].product_name + " in stock.");
+			connection.end();
+	}
 }
